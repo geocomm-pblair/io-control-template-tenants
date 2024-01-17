b9b4019603680ac144c2dcb2f169a8debc6be697
Bernardo Chiappe <bernardo.chiappe@gmail.com>
2024-01-17 18:30:34 -0300

    Adding more TF stuff

--- hello.tf
+++ hello.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "default" {
   provisioner "local-exec" {
-    command = "echo 'Hello {{name}}'"
+    command = "echo 'Hello ${var.name}'"
   }
 }
--- tag.sh
+++ tag.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-VERSION="v0.0.7"
+VERSION="v0.0.8"
 git tag -a "${VERSION}" -m "Version ${VERSION}."
 git push origin "${VERSION}"
